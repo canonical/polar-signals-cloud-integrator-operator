46d1ff41730d8ad7e79b4bb2fe4db56a450352bc
Jon Seager <jon@sgrs.uk>
2023-08-07 17:41:49 +0100

    test: s/ubuntu/ubuntu-lite/g for charm tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -27,12 +27,12 @@ async def test_deploy(ops_test: OpsTest, charm_under_test):
 @mark.abort_on_fail
 async def test_profile_store_relation(ops_test: OpsTest):
     await asyncio.gather(
-        ops_test.model.deploy("ubuntu", channel="stable"),
+        ops_test.model.deploy("ubuntu-lite", channel="stable"),
         ops_test.model.deploy("parca-agent", channel="edge"),
         ops_test.model.wait_for_idle(
-            apps=["ubuntu", "parca-agent"], status="active", raise_on_blocked=True, timeout=1000
+            apps=["ubuntu-lite", "parca-agent"], status="active", raise_on_blocked=True, timeout=1000
         ),
-        ops_test.model.relate("ubuntu", "parca-agent"),
+        ops_test.model.relate("ubuntu-lite", "parca-agent"),
     )
     await asyncio.gather(
         ops_test.model.relate(PSCLOUD, "parca-agent"),
